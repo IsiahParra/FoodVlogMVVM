984a6642d397436b69b540f861e093054b98dc89
Isiah Parra <isiahparra@Isiahs-MacBook-Pro.local>
2022-06-15 14:04:17 -0600

    first commit

---
+++ FoodVlog(MVVM)/FoodVlog(MVVM)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodVlog(MVVM)
+//
+//  Created by Isiah Parra on 6/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
